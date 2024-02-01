a2185068c227b6ccb6624dccdb456d74b9ac956d
Prashant Verma <Prashantverma0612@gmail.com>
2024-02-02 01:03:55 +0530

    login page

--- src/components/Login/Login.jsx
+++ src/components/Login/Login.jsx
@@ -7,8 +7,8 @@ const Login = () => {
       <img src={backgroundImg2} alt="img" className='w-full h-screen object-cover' />
       <div className='absolute text-center bg-[#8a2387] shadow-3xl p-5 flex flex-col w-80 top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2'>
         <h1 className='mb-3 text-white font-bold'>Login Here</h1>
-        <input type="text" placeholder='Email' className='mb-3 p-1'/>
-        <input type="password" placeholder='Password' className='p-1'/>
+        <input type="text" placeholder='Email' required className='mb-3 p-1'/>
+        <input type="password" placeholder='Password' required className='p-1'/>
         <div className='flex justify-between mt-2'>
           <span className='flex align-middle items-center'>
             <input type="checkbox" className='mr-1'/><p className='text-[12px] text-white'>Remember Me</p>
