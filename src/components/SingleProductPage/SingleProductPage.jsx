ac3214188b9d20b71c2a1a96318175c17eafa8d2
prashant0612 <Prashantverma0612@gmail.com>
2024-03-29 18:59:15 +0530

    responsive done

--- src/components/SingleProductPage/SingleProductPage.jsx
+++ src/components/SingleProductPage/SingleProductPage.jsx
@@ -46,28 +46,28 @@ const SingleProductPage = () => {
   return (
     <>
       <div className="pt-20 ">
-        <div className="w-full flex justify-around p-20 flex-wrap">
-          <div className="w-[35%] shadow-3xl ">
+        <div className="w-full flex justify-around p-20 flex-wrap max-[768px]:justify-normal max-[768px]:p-14">
+          <div className="w-[35%] shadow-3xl max-[768px]:w-full">
             <img
               src={product.img}
               alt="images"
-              className="w-full bg-cover  bg-center "
+              className="w-full bg-cover bg-center "
             />
           </div>
-          <div className="w-[40%] flex flex-col justify-around">
-            <div>
-              <h1 className="text-[60px] leading-none font-semibold">
+          <div className="w-[40%] flex flex-col justify-around max-[768px]:w-full">
+            <div className=" max-[768px]:w-full">
+              <h1 className="text-[60px] leading-none font-semibold max-[768px]:text-2xl max-[768px]:mt-10">
                 {product.title}{product.name}
               </h1>
-              <h1 className="mt-10 text-4xl font-semibold">{product.price}</h1>
+              <h1 className="mt-10 text-4xl font-semibold max-[768px]:text-xl max-[768px]:mt-5">{product.price}</h1>
               <h1>{product.old_price}</h1>
             </div>
-            <div className="flex ">
-              <button className="flex items-center bg-red-500 p-2 px-5 font-semibold text-white">
+            <div className="flex max-[768px]:justify-between max-[768px]:w-full">
+              <button className="flex items-center bg-red-500 p-2 px-5 font-semibold text-white max-[768px]:text-[11px] max-[768px]:text-nowrap ">
                 Add To Cart
-                <FaShoppingCart className="ml-2" />
+                <FaShoppingCart className="ml-2 max-[768px]:ml-0 max-[768px]:text-xl" />
               </button>
-              <button className="text-white bg-red-500 p-2 px-5 font-semibold ml-10">
+              <button className="text-white bg-red-500 p-2 px-5 font-semibold ml-10  max-[768px]:text-nowrap max-[768px]:ml-0 ">
                 Buy Now
               </button>
             </div>
