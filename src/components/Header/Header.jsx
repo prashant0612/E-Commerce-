c8962793d8d214d326484becae6a8beb0ab13e17
prashant0612 <Prashantverma0612@gmail.com>
2024-04-05 17:25:28 +0530

    lauthentication done

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -6,10 +6,13 @@ import AOS from "aos";
 import "aos/dist/aos.css";
 import { CiSearch } from "react-icons/ci";
 import { CartContext } from "../CartContext";
+import { useAuth0 } from '@auth0/auth0-react';
 
 const Header = () => {
 
   const { cartItems } = useContext(CartContext);
+
+  const { loginWithRedirect, logout, isAuthenticated, user} = useAuth0();
 
   return (
     <div className=' w-full h-20 bg-gradient-to-r from-purple-600 via-red-500 to-yellow-300 flex justify-between align-middle items-center fixed z-10 max-[768px]:flex-col max-[768px]:h-auto max-[768px]:w-full'>
@@ -50,9 +53,19 @@ const Header = () => {
         </li>
         <Link to="/cart"> <FaCartArrowDown className='mr-10 text-2xl hidden max-[768px]:mr-0 max-[768px]:block'/></Link>
       </ul>
+      {isAuthenticated &&
+        <div className='flex align-middle items-center'>
+          <p className='text-purple-700 font-semibold'>Welcome<span className='font-bold text-black ml-4'>{user.name}</span></p>
+          <img src={user.picture} alt="image" className='w-10 rounded-full ml-3'/>
+        </div>     
+      }
       <div className='flex items-center'>
-        <Link to= "/login"> <button className=' mr-12 bg-red-600 p-2 px-5 rounded-2xl text-white font-bold max-[768px]:hidden'>Login</button></Link>
 
+        {isAuthenticated ?
+        (<button className=' mr-12 bg-red-600 p-2 px-5 rounded-2xl text-white font-bold max-[768px]:hidden' onClick={() => logout({ logoutParams: { returnTo: window.location.origin } })}>LogOut</button>)
+        :(<button className=' mr-12 bg-red-600 p-2 px-5 rounded-2xl text-white font-bold max-[768px]:hidden' onClick={()=> loginWithRedirect()}>Login</button>)
+        }
+         
         <Link to="/cart">
          <span className='text-xl font-extrabold text-purple-600 absolute top-1 right-11 z-10'>{cartItems.length}</span>
          <FaCartArrowDown className='mr-10 text-2xl max-[768px]:hidden relative'/>
--- src/main.jsx
+++ src/main.jsx
@@ -1,69 +1,64 @@
-import React, { useState,useContext } from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import { RouterProvider, createBrowserRouter } from 'react-router-dom';
-import Layout from './Layout.jsx';
-import Shop from './components/Shop/Shop.jsx';
-import Men from './components/Men/Men.jsx';
-import Women from './components/Women/Women.jsx';
-import Kids from './components/Kids/Kids.jsx';
-import Login from './components/Login/Login.jsx';
-import Cart from './components/Cart/Cart.jsx'; 
-import SingleProductPage from './components/SingleProductPage/SingleProductPage.jsx';
-import { CartProvider } from './components/CartContext.jsx';
-import Sign from './components/Sign-up/Sign.jsx';
+import React, { useState, useContext } from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import { RouterProvider, createBrowserRouter } from "react-router-dom";
+import Layout from "./Layout.jsx";
+import Shop from "./components/Shop/Shop.jsx";
+import Men from "./components/Men/Men.jsx";
+import Women from "./components/Women/Women.jsx";
+import Kids from "./components/Kids/Kids.jsx";
+import Cart from "./components/Cart/Cart.jsx";
+import SingleProductPage from "./components/SingleProductPage/SingleProductPage.jsx";
+import { CartProvider } from "./components/CartContext.jsx";
+import { Auth0Provider } from '@auth0/auth0-react';
 
 const App = () => {
-  
-
   const router = createBrowserRouter([
     {
-      path: 'login',
-      element: <Login />
-    },
-    {
-      path: 'signup',
-      element: <Sign/>
-    },
-    {
-      path: '/',
+      path: "/",
       element: <Layout />,
       children: [
         {
-          path: '',
-          element: <Shop />
+          path: "",
+          element: <Shop />,
         },
         {
           path: "Men",
-          element: <Men />
+          element: <Men />,
         },
         {
-          path: 'Women',
-          element: <Women />
+          path: "Women",
+          element: <Women />,
         },
         {
           path: "Kids",
-          element: <Kids />
+          element: <Kids />,
         },
         {
-          path: 'Cart',
-          element: <Cart/>
+          path: "Cart",
+          element: <Cart />,
         },
         {
-          path: '/singlePage/:productId',
-          element: <SingleProductPage />
-        }
-      ]
-    }
+          path: "/singlePage/:productId",
+          element: <SingleProductPage />,
+        },
+      ],
+    },
   ]);
 
   return (
-    <React.StrictMode>
+    <Auth0Provider
+      domain="dev-vztf56eskda1aufj.us.auth0.com"
+      clientId="XAzZiy2JqSkX7Dp5yYdoT0hwObKxsLQ0"
+      authorizationParams={{
+        redirect_uri: window.location.origin,
+      }}
+    >
       <CartProvider>
         <RouterProvider router={router} />
       </CartProvider>
-    </React.StrictMode>
+    </Auth0Provider>
   );
 };
 
-ReactDOM.createRoot(document.getElementById('root')).render(<App />);
+ReactDOM.createRoot(document.getElementById("root")).render(<App />);
